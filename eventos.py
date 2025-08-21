2a0c650dc6f68afdc03716818b101b6c0aafba62
Mile Dawi <miledawi@gmail.com>
2025-08-21 16:40:25 -0300

    Funciones aclaradas

--- eventos.py
+++ eventos.py
@@ -1,1 +1,11 @@
-sdfdsjufids
+#funcionones 
+
+# crear un evento - artista, estadio, fechas, hora, precio, cantidad de entradas
+# modificar un evento
+# modificar fecha
+# eliminar un evento
+# Vender entrada
+# Cancelar entrada
+# Ver entradas vendidas
+# Analisis de datos - promedio de entradas vendidas, total de entradas vendidas, eventos mas vendidos
+
