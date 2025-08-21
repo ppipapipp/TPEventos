e04ab12460b7748d7c154741eea47fe0fd233efd
Coni <constanza.a.escri@gmail.com>
2025-08-21 16:49:18 -0300

    edhewjh

--- eventos.py
+++ eventos.py
@@ -9,3 +9,21 @@
 # Cancelar entrada
 # Ver entradas vendidas
 # Analisis de datos - promedio de entradas vendidas, total de entradas vendidas, eventos mas vendidos
+
+print("sistema de registro de venta de entradas")
+#inicio menu
+opcion_menu=int(input(""" elija la opcion que corresponda:
+                      1.- Venta de entrada
+                      2.- Gestion de eventos
+                      3.- Ayuda
+                      """))
+while opcion_menu!=-1:
+
+    if opcion_menu==1:
+        x
+    elif opcion_menu==2:
+        x
+    elif opcion_menu==3:
+        x
+    else:
+        print(" opcion inexistente ")
